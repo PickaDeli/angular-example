f6a6e388349986740119b209ffd6999911c4c6f3
Sari <s.meuronen@me.com>
2025-10-03 09:24:15 +0300

    Created a unit test for routing from home page to feedback page.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,13 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
+import { provideRouter, Router } from '@angular/router';
 //import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let router: Router;
+  let routerSpy: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        provideRouter([{ path: 'home', component: HomeComponent }]),
+      ],
       imports: [HomeComponent],
       /*schemas: [
         CUSTOM_ELEMENTS_SCHEMA
@@ -17,10 +23,21 @@ describe('HomeComponent', () => {
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
+
+    router = TestBed.inject(Router);
+    routerSpy = spyOn(router, 'navigate');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should navigate to Feedback', () => {
+    component.showFeedbackPage();
+
+    expect(routerSpy).toHaveBeenCalledWith(['feedback']);
+  });
+
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,13 +4,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 
 @Component({
-    selector: 'app-home',
-    imports: [
-        MatButtonModule,
-        MatListModule,
-    ],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+  selector: 'app-home',
+  imports: [
+    MatButtonModule,
+    MatListModule,
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
 })
 export class HomeComponent {
   headerText = 'Testing Angular Applications';
@@ -19,6 +19,8 @@ export class HomeComponent {
 
   ngOnInit(): void {
   }
+
+  //TODO #6
 
   showFeedbackPage(): void {
     this.router.navigate(['feedback']);
